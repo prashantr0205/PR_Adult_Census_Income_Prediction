9de1587f848020e4c7e1dad7bcd7ebb9c5964fc8
prashant <prashant.r0205@gmail.com>
2022-07-19 19:04:38 +0530

    running

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ from census.exception import CensusException
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
+
 
 def index():
     try:
         raise Exception("We are testing custom exception")
     except Exception as e:
-        census=CensusException(e,sys) from e
-    logging.info("We are testing logging module")
+        census=CensusException(e,sys) 
+        logging.info("census.error_message")
+        logging.info("We are testing logging module")
     return"CICD pipeline has been created"
 if __name__=="__main__":
     app.run(debug=True)
