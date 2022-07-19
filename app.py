7e57a3b0114769971c952b9e64451ce67d37e920
prashant <prashant.r0205@gmail.com>
2022-07-19 18:41:38 +0530

    exception folder created

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import sys
 from census.logger import logging
+from census.exception import CensusException
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        census=CensusException(e,sys) from e
     logging.info("We are testing logging module")
     return"CICD pipeline has been created"
 if __name__=="__main__":
